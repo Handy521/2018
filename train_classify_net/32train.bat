33629d8b0d679ca7668d308d616fa4b0ddf4ce29
Handy521 <33478085+Handy521@users.noreply.github.com>
2019-04-16 17:43:34 +0800

    需要先安装caffe,也可以用tensorfolw训练

---
+++ train_classify_net/32train.bat
@@ -0,0 +1,2 @@
+H:\caffe-master\Build\x64\Release\caffe.exe  train --solver=3con2fc_solver.prototxt
+pause
---
+++ train_classify_net/conv_lmdb.bat
@@ -0,0 +1,3 @@
+SET GLOG_logtostderr=1
+H:\caffe-master\Build\x64\Release\convert_imageset.exe --backend="lmdb" H:\rotate\ shuttf-train0123.txt lmdb_train2 
+pause
